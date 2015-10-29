a1e95d300471621a88909961a723d3ad397c51f3
Gilbert <gyim@hawaii.edu>
2015-10-28 19:07:42 -1000

    Fixed insecure and autopublish removal

--- collections/polls.js
+++ collections/polls.js
@@ -2,3 +2,15 @@
  * Created by Gilbert on 10/28/2015.
  */
 Polls = new Mongo.Collection('polls');
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  Meteor.publish("polls", function () {
+    return Polls.find();
+  });
+}
+
+if (Meteor.isClient) {
+  // This code only runs on the client
+  Meteor.subscribe("polls");
+}
